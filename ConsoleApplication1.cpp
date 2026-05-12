657305fc9952f256f6bb82649602dd6a341d85de
あ熊木拓斗 <kdt25gpr30004@edu.kobe-tech.ac.jp>
2026-05-12 15:08:44 +0900

    お試し

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -5,7 +5,9 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    for (int t = 0; t < 100;t++) {
+        std::cout << "Hello World!\n";
+    }
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
